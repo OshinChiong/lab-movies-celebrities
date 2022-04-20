d6cdaff834ad29c1d89a55a525d83a9d415e8e2c
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-04-20 14:34:22 -0400

    done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,24 +1,37 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-const express = require("express");
 const Celebrity = require("../models/Celebrity.model");
+
 // all your routes here
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then((allCelebrities) => {
+      res.render("celebrities/celebrities", { allCelebrities: allCelebrities });
+    })
+    .catch((error) => {
+      console.log("Failure", error.message);
+    });
+});
 
 router.get("/create", (req, res, next) => {
-    res.render("celebrities/new-celebrity");
-  });
-  
-  router.post("/create", (req, res, next) => {
-    Celebrity.create({ ...req.body })
-      .then(function (results) {
-        res.redirect("/celebrities");
-      })
-      .catch(function (error) {
-        res.redirect("/celebrities/new-celebrity");
-      });
-  });
-  
+  console.log("This is in the celebrities route");
+  res.render("celebrities/new-celebrity");
+});
 
-
+router.post("/create", (req, res, next) => {
+  console.log("About to create celeb", req.body);
+  Celebrity.create({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  })
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+      console.log("Failed", error.message);
+      res.render("celebrities/new-celebrity");
+    });
+});
 
 module.exports = router;
